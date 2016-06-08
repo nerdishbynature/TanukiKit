08f222d0020d79a94cf5f41a8a5ceeedd9d24df7
Tiago Ferreira <me@tiferrei.com>
2016-06-08 16:27:13 +0100

    Reset User file for pull request

--- TanukiKit/User.swift
+++ TanukiKit/User.swift
@@ -3,24 +3,14 @@ import RequestKit
 
 // MARK: model
 @objc public class User: NSObject {
-    public var id: Int
+    public let id: Int
     public var login: String?
     public var bio: String?
     public var name: String?
     public var email: String?
     public var privateToken: String?
-    public var avatarURL: String?
-    public var webURL: String?
-    public var websiteURL: String?
-    public var skype: String?
-    public var linkedin: String?
-    public var twitter: String?
-    public var isAdmin: Bool?
-    public var projectsLimit: Int?
-    public var canCreateProject: Bool?
-    public var canCreateGroup: Bool?
-    
-    public init(_ json: [String : AnyObject]) {
+
+    public init(_ json: [String: AnyObject]) {
         if let id = json["id"] as? Int {
             self.id = id
             login = json["username"] as? String
@@ -28,16 +18,6 @@ import RequestKit
             name = json["name"] as? String
             email = json["email"] as? String
             privateToken = json["private_token"] as? String
-            avatarURL = json["avatar_url"] as? String
-            webURL = json["web_url"] as? String
-            websiteURL = json["website_url"] as? String
-            skype = json["skype"] as? String
-            linkedin = json["linkedin"] as? String
-            twitter = json["twitter"] as? String
-            isAdmin = json["is_admin"] as? Bool
-            projectsLimit = json["projects_limit"] as? Int
-            canCreateProject = json["can_create_project"] as? Bool
-            canCreateGroup = json["can_create_group"] as? Bool
         } else {
             id = -1
         }
@@ -47,14 +27,14 @@ import RequestKit
 // MARK: request
 
 public extension TanukiKit {
-    
+
     /**
      Fetches the currently logged in user
      - parameter completion: Callback for the outcome of the fetch.
      */
-    public func me(completion: (response: Response <User> ) -> Void) {
+    public func me(completion: (response: Response<User>) -> Void) {
         let router = UserRouter.ReadAuthenticatedUser(self.configuration)
-        router.loadJSON([String : AnyObject].self) { json, error in
+        router.loadJSON([String: AnyObject].self) { json, error in
             if let error = error {
                 completion(response: Response.Failure(error))
             } else {
@@ -71,28 +51,28 @@ public extension TanukiKit {
 
 enum UserRouter: Router {
     case ReadAuthenticatedUser(Configuration)
-    
+
     var configuration: Configuration {
         switch self {
         case .ReadAuthenticatedUser(let config): return config
         }
     }
-    
+
     var method: HTTPMethod {
         return .GET
     }
-    
+
     var encoding: HTTPEncoding {
         return .URL
     }
-    
+
     var path: String {
         switch self {
         case .ReadAuthenticatedUser:
             return "user"
         }
     }
-    
+
     var params: [String: String] {
         return [:]
     }
