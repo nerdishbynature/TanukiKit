b721a388d014c32de533504081f108a161cb093e
Tiago Ferreira <me@tiferrei.com>
2016-06-28 16:01:42 +0100

    Adds additional fields to User model
    User Request Improvements
    
    Improvements and NSDate/NSURL adaptions (FIXME)
    
    Fixed NSDate/NSURL parsing
    
    Adds additional fields to User model
    
    remove print

--- TanukiKit/User.swift
+++ TanukiKit/User.swift
@@ -3,20 +3,66 @@ import RequestKit
 
 // MARK: model
 @objc public class User: NSObject {
-    public let id: Int
-    public var login: String?
-    public var bio: String?
     public var name: String?
+    public var login: String?
+    public let id: Int
+    public var state: String?
+    public var avatarURL: NSURL?
+    public var webURL: NSURL?
+    public var createdAt: NSDate?
+    public var isAdmin: Bool?
+    public var bio: String?
+    public var location: String?
+    public var skype: String?
+    public var linkedin: String?
+    public var twitter: String?
+    public var websiteURL: NSURL?
+    public var lastSignInAt: NSDate?
+    public var confirmedAt: NSDate?
     public var email: String?
+    public var themeId: Int?
+    public var colorSchemeId: Int?
+    public var projectsLimit: Int?
+    public var currentSignInAt: NSDate?
+    public var canCreateGroup: Bool?
+    public var canCreateProject: Bool?
+    public var twoFactorEnabled: Bool?
+    public var external: Bool?
     public var privateToken: String?
 
     public init(_ json: [String: AnyObject]) {
         if let id = json["id"] as? Int {
-            self.id = id
-            login = json["username"] as? String
-            bio = json["bio"] as? String
             name = json["name"] as? String
+            login = json["username"] as? String
+            self.id = id
+            state = json["state"] as? String
+            if let urlString = json["avatar_url"] as? String, url = NSURL(string: urlString) {
+                avatarURL = url
+            }
+            if let urlString = json["web_url"] as? String, url = NSURL(string: urlString) {
+                webURL = url
+            }
+            createdAt = Time.rfc3339Date(json["created_at"] as? String)
+            isAdmin = json["is_admin"] as? Bool
+            bio = json["bio"] as? String
+            location = json["location"] as? String
+            skype = json["skype"] as? String
+            linkedin = json["linkedin"] as? String
+            twitter = json["twitter"] as? String
+            if let urlString = json["website_url"] as? String, url = NSURL(string: urlString) {
+                websiteURL = url
+            }
+            lastSignInAt = Time.rfc3339Date(json["last_sign_in_at"] as? String)
+            confirmedAt = Time.rfc3339Date(json["confirmed_at"] as? String)
             email = json["email"] as? String
+            themeId = json["theme_id"] as? Int
+            colorSchemeId = json["color_scheme_id"] as? Int
+            projectsLimit = json["projects_limit"] as? Int
+            currentSignInAt = Time.rfc3339Date(json["current_sign_in_at"] as? String)
+            canCreateGroup = json["can_create_group"] as? Bool
+            canCreateProject = json["can_create_project"] as? Bool
+            twoFactorEnabled = json["two_factor_enabled"] as? Bool
+            external = json["external"] as? Bool
             privateToken = json["private_token"] as? String
         } else {
             id = -1
--- TanukiKitTests/TestHelper.swift
+++ TanukiKitTests/TestHelper.swift
@@ -10,4 +10,20 @@ internal class TestHelper {
         }
         return nil
     }
+
+    internal class func parseDate(date: String?) -> NSDate? {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        let dateOutput = dateFormatter.dateFromString(date!)
+        return dateOutput
+    }
+    
+    internal class func JSONFromFile(name: String) -> AnyObject {
+        let bundle = NSBundle(forClass: self)
+        let path = bundle.pathForResource(name, ofType: "json")!
+        let data = NSData(contentsOfFile: path)!
+        let dict: AnyObject? = try? NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers)
+        return dict!
+    }
 }
